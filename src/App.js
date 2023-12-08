8c87718d9f9af830dc38437f7c3c7b2f0d756ae6
Adan <adan.marchena@hotmail.com>
2023-12-07 20:33:21 -0400

    Guardando avance

--- src/App.js
+++ src/App.js
@@ -1,9 +1,40 @@
 import './App.css';
+import Boton from './components/Boton';
+import Contador from './components/Contador';
+import FreeCodeCampLogo from './img/descarga.png';
+import { useState } from 'react';
 
 function App() {
+
+  const [numClics, setNUmClics] = useState(0);
+
+  const manejarClic = () => {
+    setNUmClics(numClics+1);
+  };
+
+  const reiniciarContador = () => {
+    setNUmClics(0);
+  };
+
   return (
     <div className="App">
-      
+      <div className='freecodecamp-logo-contenedor'>
+        <img className='freecodecamp-logo'
+          src={FreeCodeCampLogo}
+          alt='logo de freeCodeCamp' />
+      </div>
+      <div className='contenedor-principal'>
+        <Contador 
+          numClics= {numClics} />
+        <Boton 
+          texto='Clic'
+          esBotonDeClic={true}
+          manejarClic={manejarClic}/>
+        <Boton 
+          texto='Reiniciar'
+          esBotonDeClic={false}
+          manejarClic={reiniciarContador}/>
+      </div>
     </div>
   );
 }
